1352ddf8176aa602e495441df4aecd2b759a450e
cphan31 <cphan31@gatech.edu>
2020-02-13 22:02:19 -0500

    attmept to read from db

--- routes/searchEvent.js
+++ routes/searchEvent.js
@@ -1,27 +1,44 @@
 var firebase = require('firebase');
-require('firebase/auth');
 require('firebase/firestore');
 var db = require('./firebase'); //get reference to firebase config file
 
 var express = require('express');
 var router = express.Router();
 
-const dbRef = firebase.database().ref("events");//declare database using app initialization in firebase.js
 
+var database = firebase.firestore(db.app); //declare database using app initialization in firebase.js
+
+const dbRef = firebase.database().ref("events");//declare database using app initialization in firebase.js
 
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  console.log(req.body);
+router.get('/', async (req, res) => {
+    const city = req.body.city
+    var userReference = firebase.database().ref("/events/");
 
-  var temp = req.body;
-  dbRef.on("value", snap => {
-    console.log(snap); // this key will output users
-    console.log(snap.key); // this method will return full user
-  });
-  //res.send(temp.city);
+	//Attach an asynchronous callback to read the data
+	userReference.on("value", 
+			  function(snapshot) {
+					console.log(snapshot.val());
+					res.json(snapshot.val());
+					userReference.off("value");
+					}, 
+			  function (errorObject) {
+					console.log("The read failed: " + errorObject.code);
+					res.send("The read failed: " + errorObject.code);
+			 });
 });
+
+const getEventsByCity = async (id) =>  {
+    const doc = await database.doc('events').get()
+    const data = doc.data()
+    if (!data) {
+      console.error('member does not exist')
+      return
+    }
+    return data
+  }
 
 
 module.exports = router;
